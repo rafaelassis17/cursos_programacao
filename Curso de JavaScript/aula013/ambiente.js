3105f934e1cfce0fa6c58bef37e4869cdbb67043
rafael_assis <rafael7876578@gmail.com>
2026-04-16 18:25:14 -0400

    atualizar
    b

---
+++ Curso de JavaScript/aula013/ambiente.js
@@ -0,0 +1,26 @@
+let c = 1
+do {
+    console.log(`Olá, mundo! ${c}`)
+    c++
+}while (c <= 6)
+
+
+/*while (c <= 6){
+    console.log(`Olá, mundo! ${c}`)
+    c++
+}*/
+
+
+
+
+
+
+
+
+/*console.log('Olá, mundo!')
+console.log('Olá, mundo!')
+console.log('Olá, mundo!')
+console.log('Olá, mundo!')
+console.log('Olá, mundo!')
+console.log('Olá, mundo!')
+console.log('Olá, mundo!')*/
---
+++ Curso de JavaScript/aula014/ambiente.js
@@ -0,0 +1,6 @@
+console.log('Vai começar...')
+
+for(let c = 1; c <=5; c++ ){
+    console.log(c)
+}
+console.log("FIM!!!!")
