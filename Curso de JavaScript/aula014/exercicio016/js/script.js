9390e577fff735b4060370b709f45ad9755e3552
rafael_assis <rafael7876578@gmail.com>
2026-04-18 18:15:05 -0400

    comitec
    s

---
+++ Curso de JavaScript/aula014/exercicio016/js/script.js
@@ -0,0 +1,35 @@
+function contar() {
+  let inicio = document.getElementById("txti");
+  let fim = document.getElementById("txtf");
+  let passo = document.getElementById("txtp");
+  let res = document.getElementById("res");
+
+  if (
+    inicio.value.length == 0 ||
+    fim.value.length == 0 ||
+    passo.value.length == 0
+  ) {
+    res.innerHTML = 'impossível contar!'
+    alert("erro faltam dados");
+    
+  } else {
+    res.innerHTML = "contando: <br>";
+    let i = Number(inicio.value);
+    let f = Number(fim.value);
+    let p = Number(passo.value);
+    if (p <= 0 ){
+        alert('Passo inválido considerando passo 1')
+        p =1
+    }
+    if (i < f) {
+      for (let c = i; c <= f; c += p) {
+        res.innerHTML += ` ${c} \u{1F449}`;
+      }
+     
+    } else {
+        for (let c =1; c >=f; c-=p){
+            res.innerHTML += `${c} \u{1F449}`
+        }
+    } res.innerHTML += `\u{1F3c1}`;
+  }
+}
