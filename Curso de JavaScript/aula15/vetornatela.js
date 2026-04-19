231359947589b5edcf03e65428b3f2a4333513d3
rafael_assis <rafael7876578@gmail.com>
2026-04-19 19:38:23 -0400

    flex box
    s

---
+++ Curso de JavaScript/aula15/vetornatela.js
@@ -0,0 +1,8 @@
+let valores = [8, 1, 7, 4, 2, 9]
+
+for(let pos in valores) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
